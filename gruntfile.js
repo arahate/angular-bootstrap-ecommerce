57831d5bb4af8a664bb9249f489080a11cb33f18
Rahate <abhijit.rahate@capgemini.com>
2017-06-13 16:55:08 +0530

    Integrating nodemon to grunt and setting up migration strategy

--- gruntfile.js
+++ gruntfile.js
@@ -2,29 +2,35 @@ module.exports = function (grunt) {
 	
 	grunt.initConfig({
 			pkg: grunt.file.readJSON('package.json'),
-			express: {
-					dev: {
-						options: {
-						port: 9000,
-        				bases: ['app', 'node_modules','theme']
-						}
-					},
-					prod: {
-						options: {
-							script: 'server/prod/server.js',
-							node_env: 'production'
-						}
-					},
-					test: {
-						options: {
-							script: 'server/test/server.js'
-						}
-					}
+			// express: {
+			// 		dev: {
+			// 			options: {
+			// 			port: 9000,
+        	// 			bases: ['app', 'node_modules','theme']
+			// 			}
+			// 		},
+			// 		prod: {
+			// 			options: {
+			// 				script: 'server/prod/server.js',
+			// 				node_env: 'production'
+			// 			}
+			// 		},
+			// 		test: {
+			// 			options: {
+			// 				script: 'server/test/server.js'
+			// 			}
+			// 		}
+			// 	},
+			nodemon : {
+				dev: {
+        			script: 'server/dev/server.js'
 				}
+			}
 	});	
 
+	grunt.loadNpmTasks('grunt-nodemon');
 	grunt.loadNpmTasks('grunt-express-server');
 	grunt.loadNpmTasks('grunt-express');
-	grunt.registerTask('default', ['express:dev','express-keepalive']);
+	grunt.registerTask('default', ['nodemon']);
 };
 
--- server/dev/server.js
+++ server/dev/server.js
@@ -1,11 +1,34 @@
 var express =require ('express');
 var path = require ('path');
 var app =express();
+
+var mongoose = require('mongoose');                     // mongoose for mongodb
+var morgan = require('morgan');             // log requests to the console (express4)
+var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
+var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
+
+mongoose.connect('mongodb://localhost:27017/e-commerce');     // connect to mongoDB database on modulus.io
+
 var rootpath= path.normalize( __dirname + '/../../../');
 
-app.use(express.static(rootpath + '/app'));
+app.use(express.static('app'));
+app.use(express.static('node_modules'));
+app.use(express.static('theme'));
+ // set the static files location /public/img will be /img for users
+
+app.use(morgan('dev'));                                         // log every request to the console
+app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
+app.use(bodyParser.json());                                     // parse application/json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+app.use(methodOverride());  
 
 
-app.listen(8080);
-console.log('Started listening at port 8080...');
-console.log('Started listening at port 8080...');
+app.listen(3000);
+console.log('Started listening at port 3000...');
+
+
+
+
+
+
+    
